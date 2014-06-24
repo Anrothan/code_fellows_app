0e463df5ce982fdcdb2db85edc3062fe3286313f
Derek Maffett <derekgmaffett@gmail.com>
2014-06-23 20:55:44 -0700

    add index on new user sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,27 @@
 class UsersController < ApplicationController
 
 	def new
+		@user_list = User.all
 		@user = User.new
+
 	end
 
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			redirect_to root_path
-		else 
+			# Flash message confirming success
+			redirect_to signup_path
+		else
 			render 'new'
 		end
+	end
+
+	def index
+		@user_list = User.all # This is an array
+	end
+
+	def show
+		@user = User.find(params[:id])
 	end
 
 	private
