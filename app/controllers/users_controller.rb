33079b54a239a32b378480cca944edc5b8bcdd32
Derek Maffett <derekgmaffett@gmail.com>
2014-06-29 02:14:30 -0700

    Add sign-up and sign-in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 		@user_list = User.all 
 		@user = User.new(user_params)
 		if @user.save
+			sign_in @user
 			flash[:success] = "New user successfully registered"
 			redirect_to signup_path
 		else
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625010101) do
+ActiveRecord::Schema.define(version: 20140629081802) do
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20140625010101) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "last_name"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
